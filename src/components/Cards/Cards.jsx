6255c781b08cf0406433f008fca97fa1d6bb45bb
walid <walidsharar@gmail.com>
2022-05-11 07:15:06 +0200

    Card component created

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React from 'react';
+import { Typography, Grid } from '@material-ui/core';
+import CardComponent from './Card/Card';
+import styles from './Cards.module.css';
 
-const Cards = () => {
-    return(
-        <h1>Cards</h1>
-    )
-}
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Loading...';
+  }
 
-export default Cards;
+  return (
+    <div className={styles.container}>
+        <Typography gutterBottom variant="h4" component="h2">Global</Typography>
+      <Grid container spacing={3} justify="center">
+        <CardComponent
+          className={styles.infected}
+          cardTitle="Infected"
+          value={confirmed.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of active cases from COVID-19."
+        />
+        <CardComponent
+          className={styles.recovered}
+          cardTitle="Recovered"
+          value={recovered.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of recoveries from COVID-19."
+        />
+        <CardComponent
+          className={styles.deaths}
+          cardTitle="Deaths"
+          value={deaths.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of deaths caused by COVID-19."
+        />
+      </Grid>
+    </div>
+  );
+};
+
+export default Info;

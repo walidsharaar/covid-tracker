03e44dc8030f6850b96f9c485a277371298472bd
walid <walidsharar@gmail.com>
2022-05-11 06:48:03 +0200

    project created

---
+++ README.old.md
@@ -0,0 +1,7 @@
+# covid-tracker
+Covid19 tracking system using reactjs
+
+
+## api used
+
+https://covid19.mathdro.id/api
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class App extends React.Component{
+    render(){
+        <div>
+            <h1>App</h1>
+        </div>
+    }
+}
